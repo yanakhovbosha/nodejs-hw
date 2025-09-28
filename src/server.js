bbce6987b7b5f415379aabc7ef96e35fc27aa19c
Yana Hovbosha <aemec4373@gmail.com>
2025-09-28 19:23:51 +0300

    fix: corrected error name

--- src/server.js
+++ src/server.js
@@ -45,7 +45,7 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   console.error('Error', err.message);
   res.status(500).json({
-    error: err.message,
+    message: err.message,
   });
 });
 
