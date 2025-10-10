4583123d20d5f40bf43d4240360556f106e551e7
Yana Hovbosha <aemec4373@gmail.com>
2025-10-10 19:55:01 +0300

    fixed bugs in note.js and notesValidation.js files

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { model, Schema } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -16,18 +17,7 @@ const noteSchema = new Schema(
     tag: {
       type: String,
       required: false,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: [...TAGS],
       default: 'Todo',
     },
   },
