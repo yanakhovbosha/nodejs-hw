8f11fdf8509cbdb2b63bac20eff50685f879ec87
Yana Hovbosha <aemec4373@gmail.com>
2025-10-17 13:02:34 +0300

    fixed bugs in files

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ import { errors } from 'celebrate';
 import authRoutes from './routes/authRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
